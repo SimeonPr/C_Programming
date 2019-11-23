83002636b5c471a6f852f604b7298bee79896e4b
Simeon Prause <simeon@lazar.fritz.box>
2019-11-24 00:02:52 +0100

    Added option to evaluate the same polynomial multiple times.

--- main.c
+++ main.c
@@ -13,11 +13,21 @@ int main() {
     int order;
     double x;
     double * factors = get_input(&order);
+    
     printpoly(factors, order);
-    printf("At what value of x would you like to evaluate the polynomial? ");
-    scanf("%lf", &x);
-    double ans = polyval(order, factors, x);
-    printf("Answer: %lf", ans);
+    
+    while (1) {
+        char tmp = '0';
+        printf("At what value of x would you like to evaluate the polynomial? ");
+        scanf("%lf", &x);
+        double ans = polyval(order, factors, x);
+        printf("Answer: %lf\n", ans);
+        while (tmp != 'y' && tmp != 'n') {
+            printf("Another value for x? (y/n) ");
+            scanf(" %c", &tmp);
+        }
+        if (tmp == 'n') break;
+        }
     free(factors);
     return 0;
     
